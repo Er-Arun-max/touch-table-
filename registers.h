86bd867cb7648e97b98c76fc82a0106df4d09764
Er-Arun-max <optimusprime5734@gmail.com>
2026-08-03 16:59:14 +0530

    Update registers.h

--- registers.h
+++ registers.h
@@ -1,1 +1,29 @@
+// =========================================================
+// 📄 DIRECT REGISTER DEFINITIONS
+// =========================================================
+#define MHD_R       0x2B
+#define NHD_R       0x2C
+#define NCL_R       0x2D
+#define FDL_R       0x2E
+#define MHD_F       0x2F
+#define NHD_F       0x30
+#define NCL_F       0x31
+#define FDL_F       0x32
+#define ELE0_T      0x41
+#define ELE0_R      0x42
+#define ELE1_T      0x43
+#define ELE1_R      0x44
+#define ELE2_T      0x45
+#define ELE2_R      0x46
+#define ELE3_T      0x47
+#define ELE3_R      0x48
+#define ELE4_T      0x49
+#define ELE4_R      0x4A
+#define ELE5_T      0x4B
+#define ELE5_R      0x4C
+#define FIL_CFG     0x5D
+#define ELE_CFG     0x5E
+
+#define TOU_THRESH  0x06
+#define REL_THRESH  0x0A
 
