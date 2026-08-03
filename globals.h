9bbd46ee3e57515524faa9d733446674ede760ae
Er-Arun-max <optimusprime5734@gmail.com>
2026-08-03 17:40:30 +0530

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,1 +1,7 @@
+CRGB leds[TOTAL_LEDS];
 
+// Persistent states across all 12 zones
+int activeLedsCount[NUM_ZONES] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool isZoneTouched[NUM_ZONES]  = {false, false, false, false, false, false, false, false, false, false, false, false};
+
+uint8_t rainbowStartHue = 0; 
